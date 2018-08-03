f6770084335959cfc51b4fa6bab9b62ff8bbaa3e
rober <zhuhongtao@silview.com>
2018-08-03 16:36:34 +0800

    增加分支，GET/POST请求

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var http = require('http');
+var url = require('url');
+var util = require('util');
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain;charset = utf-8"});
-    response.end("Hello World!世界你好！老朱你好！/n 测试VSTS同步/n 增加用户zhuhongtao@silview.com");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
-
-console.log("123456789");
+http.createServer(function(req,res){
+    res.writeHead(200,{'content-tye':'text/plain;charset=utf-8'});
+    res.end(util.inspect(url.parse(req.url,true)));
+}).listen(1337);
