5675ab5651a90e5c92e18e72a9b99863fe4b7847
rober <zhuhongtao@silview.com>
2018-08-03 13:51:25 +0800

    修改response.end("Hello World!世界你好！老朱你好！");

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain;charset = utf-8"});
-    response.end("Hello World!世界你好！");
+    response.end("Hello World!世界你好！老朱你好！");
 
 });
 
