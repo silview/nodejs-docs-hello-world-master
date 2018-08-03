3a64ec3e1034684ef9ec7dadb2a4df63764dd317
silview <robertzhu@msn.cn>
2018-08-03 11:54:09 +0800

    start learning

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain;charset = utf-8"});
+    response.end("Hello World!世界你好！");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+console.log("123456789");
