71fcc40ae8a7973be46302822136d0b02c96bf38
rober <zhuhongtao@silview.com>
2018-08-03 15:09:09 +0800

    增加response.end("Hello World!世界你好！老朱你好！/n 测试VSTS同步");

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain;charset = utf-8"});
-    response.end("Hello World!世界你好！老朱你好！");
+    response.end("Hello World!世界你好！老朱你好！/n 测试VSTS同步");
 
 });
 
