84f0b5953e5fb0dc1464ece86d9f96a3a073deb4
rober <zhuhongtao@silview.com>
2018-08-03 16:18:39 +0800

    修改 response.end("Hello World!世界你好！老朱你好！/n 测试VSTS同步");

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain;charset = utf-8"});
-    response.end("Hello World!世界你好！老朱你好！/n 测试VSTS同步");
+    response.end("Hello World!世界你好！老朱你好！/n 测试VSTS同步/n 增加用户zhuhongtao@silview.com");
 
 });
 
